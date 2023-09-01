f91f747b91c6e4a75eb7346a478b4a27599263c9
Seishiru <143763801+Seishiru@users.noreply.github.com>
2023-09-01 09:48:49 +0800

    Add files via upload

---
+++ ActivityChallenge/src/java/BankAccount.java
@@ -0,0 +1,74 @@
+public class BankAccount {
+    private int accountNumber;
+    private double accountBalance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    public BankAccount(int accountNumber, double accountBalance, String customerName, String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean deposit(double amount) {
+    if (amount > 0) {
+        accountBalance += amount;
+        System.out.println("Deposit successful. New balance: $" + accountBalance);
+        return true;
+    } else {
+        System.out.println("Deposit failed. Invalid deposit amount.");
+        return false;
+    }
+}
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= accountBalance) {
+            accountBalance -= amount;
+            System.out.println("Withdrawn $" + amount + " from the account. New balance: $" + accountBalance);
+        } else {
+            System.out.println("Insufficient funds for withdrawal.");
+        }
+    }
+}
