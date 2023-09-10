f44f0c63189ec8554065700c6fe4678cf240b591
Kotegawa <143763801+Seishiru@users.noreply.github.com>
2023-09-10 22:23:55 +0800

    Update Main.java

--- MyTime/src/mytime/Main.java
+++ MyTime/src/mytime/Main.java
@@ -1,8 +1,6 @@
-package mytime;
-
 public class Main {
     public static void main(String[] args) {
-        mytime.MyTime time = new mytime.MyTime(8, 58, 50, true); 
+        MyTime time = new MyTime(17, 58, 50, true);
 
         System.out.println("Initial 12-hour format:");
         time.displayTime12();
@@ -12,7 +10,7 @@ public class Main {
         time.tickBySecond();
         System.out.println("Time after ticking by one second:");
         time.displayTime12();
-        
+
         time.tickByHour();
         System.out.println("Time after ticking by one hour:");
         time.displayTime12();
