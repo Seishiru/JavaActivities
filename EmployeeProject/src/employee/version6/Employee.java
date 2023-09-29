b57acf4760c59c13e04a7381bbcf48dd620880b5
Kotegawa <143763801+Seishiru@users.noreply.github.com>
2023-09-29 20:14:42 +0800

    Add files via upload
    Added abstract

---
+++ EmployeeProject/src/employee/version6/Employee.java
@@ -0,0 +1,75 @@
+package employee.version6;
+
+import java.util.Date;
+
+public abstract class Employee {
+    protected int empID;
+    protected String empName;
+    protected Date empBirthDate;
+    protected Date empDateHired;
+    protected String type;
+
+    public Employee() {
+        // Default constructor
+    }
+
+    public Employee(int empID, String empName, Date empDateHired, Date empBirthDate) {
+        this.empID = empID;
+        this.empName = empName;
+        this.empDateHired = empDateHired;
+        this.empBirthDate = empBirthDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    // Make computeSalary method abstract
+    public abstract double computeSalary();
+    // Getters and setters for the protected fields
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Date getEmpBirthDate() {
+        return empBirthDate;
+    }
+
+    public void setEmpBirthDate(Date empBirthDate) {
+        this.empBirthDate = empBirthDate;
+    }
+
+    public Date getEmpDateHired() {
+        return empDateHired;
+    }
+
+    public void setEmpDateHired(Date empDateHired) {
+        this.empDateHired = empDateHired;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee ID: " + empID + "\nEmployee Name: " + empName;
+    }
+
+    // Common methods for all employees
+    public void displayInfo() {
+        System.out.println("Employee ID: " + empID);
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Date Hired: " + empDateHired);
+        System.out.println("Date of Birth: " + empBirthDate);
+    }
+    // Add other common methods as needed
+}
